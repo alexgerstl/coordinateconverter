b193e16146d101a3b3c15bf01ee55c81d5899b83
GerstlA-SJ1617 <ger17305@spengergasse.at>
2017-02-13 19:43:05 +0100

    fixed ensurer tests

--- test/test_ensurer.py
+++ test/test_ensurer.py
@@ -12,57 +12,57 @@ class EnsurerTest(unittest.TestCase):
             string = ''
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Eingabe noch nicht vollzählig' in e.message)
+            self.assertTrue(ensurer.INPUT_NOT_COMPLETE_DE in e.message)
 
     def test_ensure_mgrs_zone_invalid2(self):
         try:
             string = '3'
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Eingabe noch nicht vollzählig' in e.message)
+            self.assertTrue(ensurer.INPUT_NOT_COMPLETE_DE in e.message)
 
     def test_ensure_mgrs_zone_invalid3(self):
         try:
             string = 'a'
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Zone muss mit Zahl beginnen.' in e.message)
+            self.assertTrue(ensurer.ZONE_HAS_TO_START_WITH_NUMBER_DE in e.message)
 
     def test_ensure_mgrs_zone_invalid4(self):
         try:
             string = '33'
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Eingabe noch nicht vollzählig' in e.message)
+            self.assertTrue(ensurer.INPUT_NOT_COMPLETE_DE in e.message)
 
     def test_ensure_mgrs_zone_invalid5(self):
         try:
             string = '3a'
-            erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string)
+            erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Zone muss aus zwei Ziffern bestehen.' in e.message)
+            self.assertTrue(ensurer.ZONE_HAS_TO_CONTAIN_TWO_NUMBERS_DE in e.message)
 
     def test_ensure_mgrs_zone_invalid6(self):
         try:
             string = '33!'
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Zone muss aus zwei Ziffern und einem Buchstaben bestehen.' in e.message)
+            self.assertTrue(ensurer.ZONE_HAS_TO_CONTAIN_TWO_NUMBERS_AND_A_LETTER_DE in e.message)
 
     def test_ensure_mgrs_zone_invalid7(self):
         try:
             string = '33I'
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Invalid latitude grid-zone: I in MGRS-String (accepted values: C-X omitting I and O)'
-                            in e.message)
+            self.assertTrue(ensurer.INVALID_LAT_GRID_ZONE_DE.format('I') in e.message)
 
     def test_ensure_mgrs_zone_invalid8(self):
         try:
             string = '32X'
             erg = ensurer.ensure_it_is_a_valid_mgrs_zone(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Invalid grid zone designation: grid zone 32 X does not exist.' in e.message)
+            print e.message
+            self.assertTrue(ensurer.INVALID_MGRS_SQUARE_LETTRES_DE.format(32, 'X') in e.message)
 
     def test_ensure_mgrs_zone_valid(self):
         string = '33U'
@@ -74,28 +74,28 @@ class EnsurerTest(unittest.TestCase):
             string = '3'
             erg = ensurer.ensure_it_is_a_valid_mgrs_square(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Gitterquadrat muss mit Buchstaben beginnen.' in e.message)
+            self.assertTrue(ensurer.SQUARE_HAS_TO_BE_TWO_LETTERS_DE in e.message)
 
     def test_ensure_mgrs_square_invalid2(self):
         try:
             string = 'X'
             erg = ensurer.ensure_it_is_a_valid_mgrs_square(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Eingabe noch nicht vollzählig' in e.message)
+            self.assertTrue(ensurer.INPUT_NOT_COMPLETE_DE in e.message)
 
     def test_ensure_mgrs_square_invalid3(self):
         try:
             string = 'X1'
             erg = ensurer.ensure_it_is_a_valid_mgrs_square(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Gitterquadrat muss aus zwei Buchstaben bestehen.' in e.message)
+            self.assertTrue(ensurer.SQUARE_HAS_TO_BE_TWO_LETTERS_DE in e.message)
 
     def test_ensure_mgrs_square_invalid4(self):
         try:
             string = 'UVP'
             erg = ensurer.ensure_it_is_a_valid_mgrs_square(string, True)
         except exceptions.ParseException, e:
-            self.assertTrue('Eingabe zu lange' in e.message)
+            self.assertTrue(ensurer.INPUT_TO_LONG_DE in e.message)
 
     def test_ensure_mgrs_square_valid(self):
         string = 'UV'
